9b7a208d23e9b0982bbd64607c012f82dccadd90
Sreehitha Mudunuri <92991165+meastudent7@users.noreply.github.com>
2024-08-07 14:06:20 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,202 +1,234 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
 from django.db.models import Count
 from django.db.models import Q
-from django.shortcuts import render, redirect, get_object_or_404
+import datetime
+import xlwt
+from django.http import HttpResponse
 
+import string
+import re
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import VotingClassifier
 import numpy as np
-
 # Create your views here.
 from Remote_User.models import ClientRegister_Model,product_sentiment_analysis,detection_ratio,detection_accuracy
 
-def login(request):
 
-
-    if request.method == "POST" and 'submit1' in request.POST:
-
-        username = request.POST.get('username')
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
         password = request.POST.get('password')
-        try:
-            enter = ClientRegister_Model.objects.get(username=username,password=password)
-            request.session["userid"] = enter.id
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
 
-            return redirect('ViewYourProfile')
-        except:
-            pass
+    return render(request,'SProvider/serviceproviderlogin.html')
 
-    return render(request,'RUser/login.html')
+def View_Sentiment_Type_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'Positive'
+    print(kword)
+    obj = product_sentiment_analysis.objects.all().filter(Q(Prediction=kword))
+    obj1 = product_sentiment_analysis.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
 
-def index(request):
-    return render(request, 'RUser/index.html')
-
-def Add_DataSet_Details(request):
-
-    return render(request, 'RUser/Add_DataSet_Details.html', {"excel_data": ''})
-
-
-def Register1(request):
-
-    if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        phoneno = request.POST.get('phoneno')
-        country = request.POST.get('country')
-        state = request.POST.get('state')
-        city = request.POST.get('city')
-        address = request.POST.get('address')
-        gender = request.POST.get('gender')
-        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
-                                            country=country, state=state, city=city,address=address,gender=gender)
-
-        obj = "Registered Successfully"
-        return render(request, 'RUser/Register1.html',{'object':obj})
-    else:
-        return render(request,'RUser/Register1.html')
-
-def ViewYourProfile(request):
-    userid = request.session['userid']
-    obj = ClientRegister_Model.objects.get(id= userid)
-    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+    ratio12 = ""
+    kword12 = 'Negative'
+    print(kword12)
+    obj12 = product_sentiment_analysis.objects.all().filter(Q(Prediction=kword12))
+    obj112 = product_sentiment_analysis.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
 
 
-def Predict_Sentiment_Type(request):
-    if request.method == "POST":
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Sentiment_Type_Ratio.html', {'objs': obj})
 
-        if request.method == "POST":
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
 
-            reviewerID=request.POST.get('reviewerID')
-            Product_Id=request.POST.get('Product_Id')
-            reviewerName=request.POST.get('reviewerName')
-            reviewText=request.POST.get('reviewText')
-            overall_rating=request.POST.get('overall_rating')
-            summary=request.POST.get('summary')
-            reviewTime=request.POST.get('reviewTime')
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
 
-        df = pd.read_csv('Datasets.csv', encoding='latin-1')
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
 
-        def apply_response(Label):
-            if (Label == 0):
-                return 0  # Negative
-            elif (Label == 1):
-                return 1  # Positive
+def View_Prediction_Of_Sentiment_Type(request):
+    obj =product_sentiment_analysis.objects.all()
+    return render(request, 'SProvider/View_Prediction_Of_Sentiment_Type.html', {'list_objects': obj})
 
-        df['results'] = df['Label'].apply(apply_response)
-
-        cv = CountVectorizer()
-        X = df['reviewText']
-        y = df['results']
-
-        print("reviewText")
-        print(X)
-        print("Results")
-        print(y)
-
-        cv = CountVectorizer()
-        # X = cv.fit_transform(X)
-
-        X = cv.fit_transform(df['reviewText'].apply(lambda X: np.str_(X)))
-
-        models = []
-        from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-        X_train.shape, X_test.shape, y_train.shape
-
-        print("Naive Bayes")
-
-        from sklearn.naive_bayes import MultinomialNB
-
-        NB = MultinomialNB()
-        NB.fit(X_train, y_train)
-        predict_nb = NB.predict(X_test)
-        naivebayes = accuracy_score(y_test, predict_nb) * 100
-        print("ACCURACY")
-        print(naivebayes)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, predict_nb))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, predict_nb))
-        models.append(('naive_bayes', NB))
-
-        # SVM Model
-        print("SVM")
-        from sklearn import svm
-
-        lin_clf = svm.LinearSVC()
-        lin_clf.fit(X_train, y_train)
-        predict_svm = lin_clf.predict(X_test)
-        svm_acc = accuracy_score(y_test, predict_svm) * 100
-        print("ACCURACY")
-        print(svm_acc)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, predict_svm))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, predict_svm))
-        models.append(('svm', lin_clf))
-
-        print("Logistic Regression")
-
-        from sklearn.linear_model import LogisticRegression
-
-        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
-        y_pred = reg.predict(X_test)
-        print("ACCURACY")
-        print(accuracy_score(y_test, y_pred) * 100)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, y_pred))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, y_pred))
-        models.append(('logistic', reg))
-
-        print("Decision Tree Classifier")
-        dtc = DecisionTreeClassifier()
-        dtc.fit(X_train, y_train)
-        dtcpredict = dtc.predict(X_test)
-        print("ACCURACY")
-        print(accuracy_score(y_test, dtcpredict) * 100)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, dtcpredict))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, dtcpredict))
-        models.append(('DecisionTreeClassifier', dtc))
-
-        classifier = VotingClassifier(models)
-        classifier.fit(X_train, y_train)
-        y_pred = classifier.predict(X_test)
-
-        reviewText1 = [reviewText]
-        vector1 = cv.transform(reviewText1).toarray()
-        predict_text = classifier.predict(vector1)
-
-        pred = str(predict_text).replace("[", "")
-        pred1 = pred.replace("]", "")
-
-        prediction = int(pred1)
-
-        if (prediction == 0):
-            val = 'Negative'
-        elif (prediction == 1):
-            val = 'Positive'
-
-        print(val)
-        print(pred1)
-
-        product_sentiment_analysis.objects.create(
-        reviewerID=reviewerID,
-        Product_Id=Product_Id,
-        reviewerName=reviewerName,
-        reviewText=reviewText,
-        overall_rating=overall_rating,
-        summary=summary,
-        reviewTime=reviewTime,
-        Prediction=val)
-
-        return render(request, 'RUser/Predict_Sentiment_Type.html',{'objs': val})
-    return render(request, 'RUser/Predict_Sentiment_Type.html')
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
 
 
+def Download_Predicted_DataSets(request):
 
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Datasets.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = product_sentiment_analysis.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+
+        row_num = row_num + 1
+
+        ws.write(row_num, 0, my_row.reviewerID, font_style)
+        ws.write(row_num, 1, my_row.Product_Id, font_style)
+        ws.write(row_num, 2, my_row.reviewerName, font_style)
+        ws.write(row_num, 3, my_row.reviewText, font_style)
+        ws.write(row_num, 4, my_row.overall_rating, font_style)
+        ws.write(row_num, 5, my_row.summary, font_style)
+        ws.write(row_num, 6, my_row.reviewTime, font_style)
+        ws.write(row_num, 7, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+
+    df = pd.read_csv('Datasets.csv',encoding='latin-1')
+
+    def clean_text(text):
+        '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
+        and remove words containing numbers.'''
+        text = text.lower()
+        text = re.sub('\[.*?\]', '', text)
+        text = re.sub('https?://\S+|www\.\S+', '', text)
+        text = re.sub('<.*?>+', '', text)
+        text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+        text = re.sub('\n', '', text)
+        text = re.sub('\w*\d\w*', '', text)
+        text = re.sub('"@', '', text)
+        text = re.sub('@', '', text)
+        text = re.sub('https: //', '', text)
+        text = re.sub('Ã¢â‚¬â€', '', text)
+        text = re.sub('\n\n', '', text)
+
+        return text
+
+    df['processed_content'] = df['summary'].apply(lambda x: clean_text(x))
+
+    def apply_response(Label):
+        if (Label == 0):
+            return 0  # Negative
+        elif (Label == 1):
+            return 1  # Positive
+
+    df['results'] = df['Label'].apply(apply_response)
+
+    cv = CountVectorizer()
+    X = df['reviewText']
+    y = df['results']
+
+    print("reviewText")
+    print(X)
+    print("Results")
+    print(y)
+
+    cv = CountVectorizer()
+   # X = cv.fit_transform(X)
+
+    X = cv.fit_transform(df['reviewText'].apply(lambda X: np.str_(X)))
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    print("Extra Tree Classifier")
+    from sklearn.tree import ExtraTreeClassifier
+    etc_clf = ExtraTreeClassifier()
+    etc_clf.fit(X_train, y_train)
+    etcpredict = etc_clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, etcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, etcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, etcpredict))
+    models.append(('RandomForestClassifier', etc_clf))
+    detection_accuracy.objects.create(names="Extra Tree Classifier", ratio=accuracy_score(y_test, etcpredict) * 100)
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+
+    print("Gradient Boosting Classifier")
+
+    from sklearn.ensemble import GradientBoostingClassifier
+    clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(
+        X_train,
+        y_train)
+    clfpredict = clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, clfpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, clfpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, clfpredict))
+    models.append(('GradientBoostingClassifier', clf))
+    detection_accuracy.objects.create(names="Gradient Boosting Classifier",
+                                      ratio=accuracy_score(y_test, clfpredict) * 100)
+
+
+
+    csv_format = 'Results.csv'
+    df.to_csv(csv_format, index=False)
+    df.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
